2bb77c2a341f0ebe081d9c84ae38f23816790634
tseng91301 <tseng91301@gmail.com>
2023-11-01 18:43:09 +0800

    20231101

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask
+from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
+@app.route('/helloname', methods=['GET'])
+def helloname():
+    if request.method == 'GET': 
+        return 'Hello ' + request.values['username'] 
 @app.route('/')
 def hello():
     return 'Hello, World!'
