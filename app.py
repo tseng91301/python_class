30583461baa849ddf04334e97b4ff19cfb762c07
tseng91301 <tseng91301@gmail.com>
2023-11-01 19:17:53 +0800

    20231101

--- app.py
+++ app.py
@@ -1,13 +1,38 @@
-from flask import Flask
+from flask import Flask, abort
 from flask import render_template
 from flask import request
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import *
+import os
 
 app = Flask(__name__)
+static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
+# Channel Access Token
+line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
+# Channel Secret
+handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
 
 @app.route('/helloname', methods=['GET'])
 def helloname():
     if request.method == 'GET': 
         return 'Hello ' + request.values['username'] 
+
+# 監聽所有來自 /callback 的 Post Request
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+
 @app.route('/')
 def hello():
     return 'Hello, World!'
