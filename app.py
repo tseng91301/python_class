4b279198f9634f9cf225ff02586ccfdfd5da013c
tseng91301 <tseng91301@gmail.com>
2023-11-01 19:41:52 +0800

    20231101

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def callback():
 def handle_message(event):
     msg = event.message.text
     try:
-        answer = '"'+answer+'", received!'
+        answer = '"'+msg+'", received!'
         print(answer)
         line_bot_api.reply_message(event.reply_token, TextSendMessage(answer))
     except:
